4b7bdbe8f9aaf5ecafd13d965b44006ddfbc6a42
idkowiakp <patryk.idkowiak@snpgroup.com>
2021-08-31 13:52:57 +0200

    db config validate

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,17 +1,44 @@
 import { registerAs } from "@nestjs/config";
+import { IsPort, IsString, validateSync } from "class-validator";
+import { plainToClass } from "class-transformer";
 
-export interface DatabaseConfig {
+export class DatabaseConfig {
+  @IsString()
   host: string;
+
+  @IsPort()
   port: number;
+
+  @IsString()
   user: string;
+
+  @IsString()
   password: string;
+
+  @IsString()
   database: string;
 }
 
-export default registerAs('database', (): DatabaseConfig => ({
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-}));
+function validateDbConfig() {
+  const validatedConfig = plainToClass(
+    DatabaseConfig,
+    mapToDatabaseConfig(process.env)
+  );
+  const errors = validateSync(validatedConfig);
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
+
+const mapToDatabaseConfig = (config) => {
+  return {
+    host: config.DB_HOST,
+    port: config.DB_PORT,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DB_DATABASE
+  };
+};
+
+export default registerAs("database", (): DatabaseConfig => (validateDbConfig()));
--- src/database-module/database.providers.ts
+++ src/database-module/database.providers.ts
@@ -1,8 +1,6 @@
-import { Provider } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { DatabaseConfig } from 'src/config/db.config';
+import { DatabaseConfig } from "src/config/db.config";
 import { createConnection } from 'typeorm';
-import { DatabaseModule } from './database.module';
 
 export const databaseProviders = [
   {
@@ -14,7 +12,7 @@ export const databaseProviders = [
       createConnection({
         type: 'postgres',
         host: dbConfig.host,
-        port: dbConfig.port,
+        port: +dbConfig.port,
         username: dbConfig.user,
         password: dbConfig.password,
         database: dbConfig.database,
